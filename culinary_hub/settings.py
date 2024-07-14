3f4a6283436da1a3d682e4d0b82bf9b1e07dbf69
Olga Mageria <olgamageria@gmail.com>
2024-07-14 22:41:43 +0300

    .

--- culinary_hub/settings.py
+++ culinary_hub/settings.py
@@ -124,7 +124,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# If using during development, ensure you have this setting
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
